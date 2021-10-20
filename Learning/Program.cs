c3ac45cf42ef3e81e80ca05f1c587ef05ee285ee
SLAYER <dkslayer@mail.ru>
2021-10-20 12:06:27 +0300

    add BinaryTree practics

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static Learning.СodeFeaturesLearning.BinarySearchTree;
 
 namespace Learning
 {
@@ -7,6 +9,17 @@ namespace Learning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var intArray = new List<int> { 1, 3, 2, 3, 4, 1 };
+            var midleIndex = intArray.Count / 2;
+            var bst = new BinaryTreeNode<int>(intArray[0]);
+            for (int i = 1; i < intArray.Count; i++)
+            {
+                bst.Add(intArray[i]);
+            }
+            bst.PrintTreeOnConsole();
+            Console.WriteLine("Element Count = "+ bst.SerchElementCount(1));
+            Console.ReadLine();
         }
     }
 }
---
+++ Learning/СodeFeaturesLearning/BinarySearchTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.СodeFeaturesLearning
+{
+    class BinarySearchTree
+    {
+        public class BinaryTreeNode<T> where T : IComparable<T>
+        {
+            private T data;
+            private BinaryTreeNode<T> leftNode;
+            private BinaryTreeNode<T> rightNode;
+            private BinaryTreeNode<T> parentNode;
+            private int elementCount = 0;
+            private StringBuilder returnStringUp = new StringBuilder();
+            private StringBuilder returnStringDown = new StringBuilder();
+            public BinaryTreeNode(T value)
+            {
+                data = value;
+            }
+            public BinaryTreeNode(T value, BinaryTreeNode<T> parent)
+            {
+                parentNode = parent;
+                data = value;
+            }
+            public void PrintTreeOnConsole() {
+                Console.WriteLine("ParentNode value = " + data);
+                
+                if (leftNode != null)
+                {
+                    Console.WriteLine();
+                    Console.Write("LeftTree: ");
+                    printTree(leftNode);
+                    Console.WriteLine();
+                    Console.WriteLine("ElementInBranhc = " + elementCount);
+                }
+                if (rightNode != null) {
+                    elementCount = 0;
+                    Console.WriteLine();
+                    Console.Write("RightTree: ");
+                    printTree(rightNode);
+                    Console.WriteLine();
+                    Console.WriteLine("ElementInBranhc = " + elementCount);
+                }
+            }
+            public int SerchElementCount(T value) {
+                int elementCounts = 0;
+                Serch(value, this);
+                void Serch(T value, BinaryTreeNode<T> node)
+                {
+                    if (node != null)
+                    {
+                        if (value.CompareTo(node.data) < 0)
+                        {
+                            Serch(value, node.leftNode);
+                        }
+                        else if (value.CompareTo(node.data) == 0)
+                        {
+                            elementCounts++;
+                            Serch(value, node.rightNode);
+                        }
+                        else
+                        {
+                            Serch(value, node.rightNode);
+                        }
+                    }
+                    else { return; }
+                }
+                return elementCounts;
+            }
+            private void printTree(BinaryTreeNode<T> node) {
+                if (node == null) return;
+                printTree(node.leftNode);
+                Console.Write(node.data + " ");
+                elementCount++;
+                if (node.rightNode != null)
+                {
+                    printTree(node.rightNode);
+                }
+            }
+            public void Add(T value) {
+                if (value.CompareTo(data) < 0)
+                {
+                    if (leftNode == null)
+                    {
+                        leftNode = new BinaryTreeNode<T>(value, this);
+                    }
+                    else { leftNode.Add(value); }
+                }
+                else {
+                    if (rightNode == null)
+                    {
+                        rightNode = new BinaryTreeNode<T>(value, this);
+                    }
+                    else { rightNode.Add(value); }
+                }
+            }
+        }
+    }
+}
